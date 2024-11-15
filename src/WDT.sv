8a8924a49707e6e9f95ffa5cda8bcd3e870e2299
Darren <ades96063@gmail.com>
2024-11-16 00:22:47 +0800

    System_Design1_v1.0_beta2
    This version: revise WDT
    Next version :
    Finish WDT, Start DMA

--- src/WDT.sv
+++ src/WDT.sv
@@ -1,13 +1,47 @@
-module WDT (
-  input     clk,  rst,
-  input     clk2, rst2,
-  input     WDEN,
-  input     WDLIVE,
-  input     [31:0]  WTOCNT,
-  output    WTO
-);
-  
+//--------------------------- Info ---------------------------//
+    //Module Name :　WDT
+    //Type        :   
+//----------------------- Environment -----------------------//
 
-
+//------------------------- Module -------------------------//
+  module WDT (
+    input     clk,  rst,
+    input     clk2, rst2,
+    input     WDEN,
+    input     WDLIVE,
+    input     [31:0]  WTOCNT,
+    output    logic   WTO
+  );
   
-endmodule
+//---------------------- parameter -------------------------//
+  logic     [1:0] S_cur, S_nxt;
+  parameter [1:0] INITIAL = 2'b00,
+                  2'b01,
+                  2'b10,
+                  2'b11;
+
+//---------------------- Main code -------------------------//
+//------------------------- FSM -------------------------//
+  
+  always_ff @( clock ) begin
+    if () S_cur <=  INITIAL;
+    else  S_cur <=  S_nxt;
+  end
+
+  always_comb begin
+    case (param)
+      : 
+      default: 
+    endcase
+  end
+//WTO
+  always_ff @(posedege clk2) begin
+    if (conditions) begin
+      WTO   <=  1'b0;  
+    end 
+    else begin
+      
+    end
+  end
+  
+  endmodule
